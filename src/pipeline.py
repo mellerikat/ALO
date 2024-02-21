8927d1fcbe72e098c610bc5bbbf449eb314e2462
wonjun.sung <wonjun.sung@lge.com>
2024-02-21 11:25:09 +0900

    feat : asset 설치 관련 코드 이동
    - src/assets.py 를 pipeline.py로 이동
    - pipeline.py에서 asset 설치 등을 관리

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,18 @@
 # set up pipeline class
+import importlib
+import sys
+import os
+import re
+from datetime import datetime
+import shutil
+from typing import Dict, List, Tuple, Union, Optional, Any
 from collections import Counter
+import git
 
 from src.constants import *
-from src.assets import *
+# from src.assets import *
+from src.install import Packages
+from src.external import ExternalHandler
 from src.logger import ProcessLogger
 
 PROC_LOGGER = ProcessLogger(PROJECT_HOME)
@@ -25,30 +35,124 @@ class AssetStructure:
 
 class Pipeline:
     # TODO ALO에 init한 class를 넘겨주면서 사용하는게 맞는건지 논의
-    def __init__(self, system_envs, install, ext_data):
+    # def __init__(self, experiment_plan: Dict, skip_mode: Dict, pipeline_type: str, redis: bool, system_envs: Dict ):
+    def __init__(self, experiment_plan: Dict, pipeline_type: str, system_envs: Dict ):
+        # self.experiment_plan = experiment_plan
+        if not os.path.exists(ASSET_HOME):
+            try:
+                os.makedirs(ASSET_HOME)
+            except: 
+                PROC_LOGGER.process_error(f"Failed to create directory: {ASSET_HOME}")
+
+        self.pipeline_type = pipeline_type
         self.system_envs = system_envs
-        # TODO asset 에 대한 클래스는 pipeline에서만 사용 나중에 옮겨야 할지 논의
-        self.asset = Assets(ASSET_HOME)
-        self.install = install
-        self.ext_data = ext_data
+        
+        # TODO ALO 에 대한 클래스는 pipeline에서만 사용 나중에 옮겨야 할지 논의
+        self.install = Packages()
+        self.external = ExternalHandler()
 
-    
-    def setup(self, pipe, experimental_plan):
-        self._empty_artifacts(pipe)
-        self._setup_asset(experimental_plan.asset_source[pipe], experimental_plan.control['get_asset_source'])
-        self._set_asset_structure(experimental_plan)
+        self.asset_structure = AssetStructure()
 
-    def load(self, pipe, experimental_plan):
+        def get_yaml_data(key, pipeline_type = 'all'): # inner func.
+            data_dict = {}
+            for data in experiment_plan[key]:
+                data_dict.update(data)
+
+            if 'train_pipeline' in data_dict and 'inference_pipeline' in data_dict:
+                if pipeline_type == 'train_pipeline':
+                    remove_key = 'inference_pipeline'
+                    del data_dict[remove_key]
+                else:
+                    remove_key = 'train_pipeline'
+                    del data_dict[remove_key]
+
+            return data_dict
+
+        # 각 key 별 value 클래스 self 변수화 --> ALO init() 함수에서 ALO 내부변수로 넘김
+        values = {}
+        for key, value in experiment_plan.items():
+            setattr(self, key, get_yaml_data(key, pipeline_type))
+
+        b = 0
+
+    def setup(self):
+        self._empty_artifacts(self.pipeline_type)
+        self._setup_asset(self.asset_source[self.pipeline_type], self.control['get_asset_source'])
+        self._set_asset_structure()
+
+        # TODO return 구성
+        # return 
+
+    def load(self):
 
         # TODO 분기 태우는 코드가 필요        
-        if pipe == 'inference_pipeline':
-            self.ext_data.external_load_model(experimental_plan.external_path, experimental_plan.external_path_permission)
+        if self.pipeline_type == 'inference_pipeline':
+            if (self.external_path['load_model_path'] != None) and (self.external_path['load_model_path'] != ""): 
+                self.external.external_load_model(self.external_path, self.external_path_permission['s3_private_key_file'])
         
         if self.system_envs['boot_on'] == False:  ## boot_on 시, skip
             # NOTE [중요] wrangler_dataset_uri 가 solution_metadata.yaml에 존재했다면,
             # 이미 _update_yaml할 때 exeternal load inference data path로 덮어쓰기 된 상태
-            self.ext_data.external_load_data(pipe, experimental_plan.external_path, experimental_plan.external_path_permission, experimental_plan.control['get_external_data'])
+            self.external.external_load_data(self.pipeline_type, self.external_path, self.external_path_permission['s3_private_key_file'], self.control['get_external_data'])
 
+        # TODO return 구성
+        # return 
+            
+    def run(self, run_step = None):
+        for step, asset_config in enumerate(self.asset_source[self.pipeline_type]):
+            PROC_LOGGER.process_info(f"==================== Start pipeline: {self.pipeline_type} / step: {asset_config['step']}")
+            self.asset_structure.args = self.get_args(step)
+            try: 
+                self.process_asset_step(asset_config, step)
+            except: 
+                PROC_LOGGER.process_error(f"Failed to process step: << {asset_config['step']} >>")
+
+    def get_args(self, step):
+        if type(self.user_parameters[self.pipeline_type][step]['args']) == type(None):
+            return dict()
+        else:
+            return self.user_parameters[self.pipeline_type][step]['args'][0]
+        
+    # def process_asset_step(self, asset_config, step, pipeline, asset_structure): 
+    def process_asset_step(self, asset_config, step): 
+        # step: int 
+        self.asset_structure.envs['pipeline'] = self.pipeline_type
+
+        _path = ASSET_HOME + asset_config['step'] + "/"
+        _file = "asset_" + asset_config['step']
+        # asset2등을 asset으로 수정하는 코드
+        _file = ''.join(filter(lambda x: x.isalpha() or x == '_', _file))
+        user_asset = self.import_asset(_path, _file)
+        if self.system_envs['boot_on'] == True: 
+            PROC_LOGGER.process_info(f"===== Booting... completes importing << {_file} >>")
+            return
+        
+        meta_dict = {'artifacts': self.system_envs['artifacts'], 'pipeline': self.pipeline_type, 'step': step, 'step_number': step, 'step_name': self.user_parameters[self.pipeline_type][step]['step']}
+        self.asset_structure.config['meta'] = meta_dict #nested dict
+
+        if step > 0: 
+            self.asset_structure.envs['prev_step'] = self.user_parameters[self.pipeline_type][step - 1]['step'] # asset.py에서 load config, load data 할때 필요 
+        self.asset_structure.envs['step'] = self.user_parameters[self.pipeline_type][step]['step']
+        self.asset_structure.envs['num_step'] = step # int  
+        self.asset_structure.envs['asset_branch'] = asset_config['source']['branch']
+
+        ua = user_asset(self.asset_structure) 
+        self.asset_structure.data, self.asset_structure.config = ua.run()
+
+        # FIXME memory release : on/off 필요 
+        try:
+            if self.control['reset_assets']:
+                self.memory_release(_path)
+                sys.path = [item for item in sys.path if self.asset_structure.envs['step'] not in item]
+            else:
+                pass
+        except:
+            self.memory_release(_path)
+            sys.path = [item for item in sys.path if self.asset_structure.envs['step'] not in item]
+        
+        PROC_LOGGER.process_info(f"==================== Finish pipeline: {self.pipeline_type} / step: {asset_config['step']}")
+
+    
     # 한번만 실행, 특정 에셋만 설치 할 수도 있음
     def _setup_asset(self, asset_source, get_asset_source):
         """asset 의 git clone 및 패키지를 설치 한다. 
@@ -77,24 +181,23 @@ class Pipeline:
             pass 
         else:
             return self._install_steps(asset_source, get_asset_source)
-        
-    def _set_asset_structure(self, experimental_plan):
+
+    def _set_asset_structure(self):
         """Asset 의 In/Out 을 data structure 로 전달한다.
         파이프라인 실행에 필요한 환경 정보를 envs 에 setup 한다.
         """
-        self.asset_structure = AssetStructure() 
         
         self.asset_structure.envs['project_home'] = PROJECT_HOME
         
         self.asset_structure.envs['solution_metadata_version'] = self.system_envs['solution_metadata_version']
         self.asset_structure.envs['artifacts'] = self.system_envs['artifacts']
         self.asset_structure.envs['alo_version'] = self.system_envs['alo_version']
-        if experimental_plan.control['interface_mode'] not in INTERFACE_TYPES:
-            self.proc_logger.process_error(f"Only << file >> or << memory >> is supported for << interface_mode >>")
-        self.asset_structure.envs['interface_mode'] = experimental_plan.control['interface_mode']
+        if self.control['interface_mode'] not in INTERFACE_TYPES:
+            PROC_LOGGER.process_error(f"Only << file >> or << memory >> is supported for << interface_mode >>")
+        self.asset_structure.envs['interface_mode'] = self.control['interface_mode']
         self.asset_structure.envs['proc_start_time'] = self.system_envs['start_time']
-        self.asset_structure.envs['save_train_artifacts_path'] = experimental_plan.external_path['save_train_artifacts_path']
-        self.asset_structure.envs['save_inference_artifacts_path'] = experimental_plan.external_path['save_inference_artifacts_path']
+        self.asset_structure.envs['save_train_artifacts_path'] = self.external_path['save_train_artifacts_path']
+        self.asset_structure.envs['save_inference_artifacts_path'] = self.external_path['save_inference_artifacts_path']
     
 
     def _install_steps(self, asset_source, get_asset_source='once'):
@@ -102,11 +205,10 @@ class Pipeline:
         for step, asset_config in enumerate(asset_source):
             # self.asset.setup_asset 기능 :
             # local or git pull 결정 및 scripts 폴더 내에 위치시킴 
-            self.asset.setup_asset(asset_config, get_asset_source)
+            self._install_asset(asset_config, get_asset_source)
             requirements_dict[asset_config['step']] = asset_config['source']['requirements']
         
         return self.install.check_install_requirements(requirements_dict) 
-    
     
     def _empty_artifacts(self, pipeline): 
         '''
@@ -125,3 +227,118 @@ class Pipeline:
                     PROC_LOGGER.process_info(f"Successfully emptied << {dir_artifacts + subdir} >> ")
         except: 
             PROC_LOGGER.process_error(f"Failed to empty & re-make << .{pipe_prefix}_artifacts >>")
+
+    def import_asset(self, _path, _file):
+        _user_asset = 'none'
+
+        try:
+            # Asset import
+            # asset_path = 상대경로로 지정 : PROJECT_HOME/scripts/data_input
+            sys.path.append(_path)
+            mod = importlib.import_module(_file)
+        except ModuleNotFoundError:
+            PROC_LOGGER.process_error(f'Failed to import asset. Not Found : {_path}{_file}.py')
+
+        # UserAsset 클래스 획득
+        _user_asset = getattr(mod, "UserAsset")
+
+        return _user_asset
+
+    def memory_release(self, _path):
+        all_files = os.listdir(_path)
+        # .py 확장자를 가진 파일만 필터링하여 리스트에 추가하고 확장자를 제거
+        python_files = [file[:-3] for file in all_files if file.endswith(".py")]
+        try:
+            for module_name in python_files:
+                if module_name in sys.modules:
+                    del sys.modules[module_name]
+        except:
+            PROC_LOGGER.process_error("An issue occurred while releasing the memory of module")
+
+    def _install_asset(self, asset_config, check_asset_source='once'): 
+        """ Description
+            -----------
+                - scripts 폴더 내의 asset들을 code가 local인지 git인지, check_asset_source가 once인지 every인지에 따라 setup  
+            Parameters
+            -----------
+                - asset_config: 현재 step의 asset config (dict 형) 
+                - check_asset_source: git을 매번 당겨올지 최초 1회만 당겨올지 ('once', 'every')
+            Return
+            -----------
+                - 
+            Example
+            -----------
+                - setup_asset(asset_config, check_asset_source='once')
+        """
+
+        # FIXME 추후 단순 폴더 존재 유무 뿐 아니라 이전 실행 yaml과 비교하여 git주소, branch 등도 체크해야함
+        def renew_asset(step_path): 
+            """ Description
+                -----------
+                    - asset을 git으로 부터 새로 당겨올지 말지 결정 
+                Parameters
+                -----------
+                    - step_path: scripts 폴더 내의 asset폴더 경로 
+                Return
+                -----------
+                    - whether_renew_asset: Boolean
+                Example
+                -----------
+                    - whether_to_renew_asset =_renew_asset(step_path) 
+            """
+            whether_renew_asset = False  
+            if os.path.exists(step_path):
+                pass
+            else: 
+                whether_renew_asset = True
+            return whether_renew_asset
+        
+        # git url 확인 -> lib
+        def is_git_url(url):
+            git_url_pattern = r'^(https?|git)://[^\s/$.?#].[^\s]*$'
+            return re.match(git_url_pattern, url) is not None
+        
+        asset_source_code = asset_config['source']['code'] # local, git url
+        step_name = asset_config['step']
+        git_branch = asset_config['source']['branch']
+        step_path = os.path.join(ASSET_HOME, asset_config['step'])
+        PROC_LOGGER.process_info(f"Start setting-up << {step_name} >> asset @ << assets >> directory.") 
+        # 현재 yaml의 source_code가 git일 땐 control의 check_asset_source가 once이면 한번만 requirements 설치, every면 매번 설치하게 끔 돼 있음 
+        ## FIXME ALOv2에서 기본으로 필요한 requirements.txt는 사용자가 알아서 설치 (git clone alov2 후 pip install로 직접) 
+        ## asset 배치 (@ scripts 폴더)
+        # local 일때는 check_asset_source 가 local인지 git url인지 상관 없음 
+        if asset_source_code == "local":
+            if step_name in os.listdir(ASSET_HOME): 
+                PROC_LOGGER.process_info(f"Now << local >> asset_source_code mode: <{step_name}> asset exists.")
+                pass 
+            else: 
+                PROC_LOGGER.process_error(f'Now << local >> asset_source_code mode: \n <{step_name}> asset folder does not exist in <assets> folder.')
+        else: # git url & branch 
+            # git url 확인
+            if is_git_url(asset_source_code):
+                # _renew_asset(): 다시 asset 당길지 말지 여부 (bool)
+                if (check_asset_source == "every") or (check_asset_source == "once" and renew_asset(step_path)): 
+                    PROC_LOGGER.process_info(f"Start renewing asset : {step_path}") 
+                    # git으로 또 새로 받는다면 현재 존재 하는 폴더를 제거 한다
+                    if os.path.exists(step_path):
+                        shutil.rmtree(step_path)  # 폴더 제거
+                    os.makedirs(step_path)
+                    os.chdir(PROJECT_HOME)
+                    repo = git.Repo.clone_from(asset_source_code, step_path)
+                    try: 
+                        repo.git.checkout(git_branch)
+                        PROC_LOGGER.process_info(f"{step_path} successfully pulled.")
+                    except: 
+                        PROC_LOGGER.process_error(f"Your have written incorrect git branch: {git_branch}")
+                # 이미 scripts내에 asset 폴더들 존재하고, requirements.txt도 설치된 상태 
+                elif (check_asset_source == "once" and not renew_asset(step_path)):
+                    modification_time = os.path.getmtime(step_path)
+                    modification_time = datetime.fromtimestamp(modification_time) # 마지막 수정시간 
+                    PROC_LOGGER.process_info(f"<< {step_name} >> asset had already been created at {modification_time}")
+                    pass  
+                else: 
+                    PROC_LOGGER.process_error(f'You have written wrong check_asset_source: {check_asset_source}')
+            else: 
+                PROC_LOGGER.process_error(f'You have written wrong git url: {asset_source_code}')
+        
+        return 
