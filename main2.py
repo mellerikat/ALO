3c75964044a98e01f9bb353862fb3e692cf781b0
wonjun.sung <wonjun.sung@lge.com>
2024-02-27 13:45:25 +0900

    feat : test용 main 코드
    - 개발 완료 후 삭제 예정

--- main2.py
+++ main2.py
@@ -1,14 +1,18 @@
 from src.utils import set_args, init_redis
 from src.alo import ALO
 
+# self.experimental_plan.user_parameters['x_col'] = 'aa'
+# pipeline = alo.pipeline()
+# pipeline.experimental_plan['input']['x_cols']
+# ***
+# pipeline.user_parameters[step_name][args]
+# 위에 내용을 작동하는 함수작성
 
 def main():
     # ALO 실행 전 필요한 args를 받아옴
     args = set_args()
-    kwargs = vars(args)
-    alo = ALO(kwargs, kwargs['config'], kwargs['mode'])
+    alo = ALO(args.config, args.system, args.mode, args.loop, args.computing)
     alo.main()
-    alo.runs()
 
 if __name__ == "__main__":
     main()
